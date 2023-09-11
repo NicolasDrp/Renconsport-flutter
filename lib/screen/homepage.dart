c3e75bbb18b054955c715b0f609df65c365bfbf5
NicolasDrp <drapier.nicolas07@gmail.com>
2023-09-11 14:35:10 +0200

    feat : AppBar (not finished)

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -8,13 +8,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isLogged = false;
+  bool isLogged = true;
   @override
   Widget build(BuildContext context) {
     if (!isLogged) {
       Navigator.pushNamed(context, '/login');
     }
     return Scaffold(
+      appBar: AppBar(
+        leading: Image.asset('assets/logo_appbar.png'),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.error_outline,
+                color: Colors.black,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings_outlined,
+                color: Colors.black,
+              ))
+        ],
+      ),bottomNavigationBar: BottomAppBar(),
       body: Column(
         children: [
           FloatingActionButton(
