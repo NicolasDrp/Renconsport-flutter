af2a454f7ab1ecba4e788834b92b87af21b61b2d
Sarah-Katz <Sarahkatz59@gmail.com>
2023-09-14 15:42:42 +0200

    feat(sessions): added page

--- lib/screen/sessions.dart
+++ lib/screen/sessions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:renconsport_flutter/widget/bottom_app_bar.dart';
+import 'package:renconsport_flutter/widget/custom_app_bar.dart';
 
 class Sessions extends StatefulWidget {
   const Sessions({super.key, required this.storage});
@@ -13,6 +15,25 @@ class Sessions extends StatefulWidget {
 class _SessionsState extends State<Sessions> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: const CustomAppbar(tutorial: ""),
+        bottomNavigationBar: const BottomAppBarWidget(),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(
+                    "Prochaine Sessions",
+                    style: TextStyle(fontSize: 26),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
