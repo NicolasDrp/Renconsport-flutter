d7726de9bc86d036761666e02b8d7919eec46a66
NicolasDrp <drapier.nicolas07@gmail.com>
2023-09-19 15:40:10 +0200

    feat : back button

---
+++ lib/widget/custom_back_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomBackButton extends StatelessWidget {
+  const CustomBackButton({super.key, required this.nav, required this.index});
+
+  final Function nav;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 80,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              nav(index);
+            },
+          ),
+        ));
+  }
+}
