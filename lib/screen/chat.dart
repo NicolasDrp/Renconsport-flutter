bc2943c5264fabf94dd1e384be41b26e0bfc9baa
Sarah-Katz <Sarahkatz59@gmail.com>
2023-09-28 16:15:51 +0200

    feat(chats): added data streaming

---
+++ lib/screen/chat.dart
@@ -0,0 +1,96 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:renconsport_flutter/services/database.dart';
+import 'package:renconsport_flutter/services/user_service.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({super.key, required this.nav});
+
+  final Function nav;
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  final TextEditingController controller = TextEditingController();
+  final Stream<QuerySnapshot> massagesStream =
+      CustomDatabase.streamData(CustomDatabase.chats);
+  String id = "ersdfftgyhujikolpm1";
+
+  @override
+  void initState() {
+    super.initState();
+    getId();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        StreamBuilder<QuerySnapshot>(
+          stream: massagesStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text("Loading");
+            }
+
+            return Expanded(
+              child: ListView(
+                children: snapshot.data!.docs
+                    .map((DocumentSnapshot document) {
+                      print(id);
+                      Map<String, dynamic> data =
+                          document.data()! as Map<String, dynamic>;
+                      return (data['sender'] == id)
+                          ? Text("A droite")
+                          : Text("A gauche");
+                    })
+                    .toList()
+                    .cast(),
+              ),
+            );
+          },
+        ),
+        TextField(
+          controller: controller,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'New message',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            sendMessage(controller.text);
+            controller.clear();
+          },
+          child: const Text('Send'),
+        ),
+      ],
+    );
+  }
+
+  void sendMessage(String text) async {
+    String idUser = await UserService.getCurrentUserId();
+    Map<String, dynamic> data = {
+      'sender': idUser,
+      'text': text,
+      'time': DateTime.now().toIso8601String(),
+    };
+    CustomDatabase.addData(CustomDatabase.chats, data);
+  }
+
+  void getId() {
+    UserService.getCurrentUserId().then((value) {
+      setState(() {
+        id = value;
+      });
+    });
+  }
+}
--- lib/screen/contacts.dart
+++ lib/screen/contacts.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:renconsport_flutter/services/user_service.dart';
 
-class Contacts extends StatefulWidget {
+class Contacts extends StatelessWidget {
   const Contacts({super.key, required this.nav});
 
   final Function nav;
 
   @override
-  State<Contacts> createState() => _ContactsState();
-}
-
-class _ContactsState extends State<Contacts> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+        onPressed: () async {
+          String idUser = await UserService.getCurrentUserId();
+          nav(8, idUser);
+        },
+        child: Icon(Icons.person_add_alt_1));
   }
 }
