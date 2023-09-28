38506fb0f7c687d71b5681445410bd27c4c34f9b
Sarah-Katz <Sarahkatz59@gmail.com>
2023-09-28 11:21:37 +0200

    feat(firebase): added firestore service

---
+++ lib/services/database.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CustomDatabase {
+  static final FirebaseFirestore database = FirebaseFirestore.instance;
+
+// Collections
+  static final CollectionReference chats = database.collection('chats');
+
+  static void addData(
+      CollectionReference collection, Map<String, dynamic> data) {
+    collection.add(data);
+  }
+
+  static List<QueryDocumentSnapshot<Object?>> readData(
+      CollectionReference collection) {
+    List<QueryDocumentSnapshot<Object?>> list = [];
+    collection.get().then((value) {
+      list = value.docs;
+    });
+    return list;
+  }
+
+  static Stream<QuerySnapshot> streamData(CollectionReference collection) {
+    String collectionName = collection.path;
+    return database.collection(collectionName).snapshots();
+  }
+}
